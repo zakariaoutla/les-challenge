d0fc92f05c65541825ceb17938295332df43777d
zakaria outla <zakariadevlop78@gmail.com>
2025-10-23 12:15:16 +0100

    add Gestion des abonnés

--- challenges/Projet Fin SAS/projet_fin_sas.js
+++ challenges/Projet Fin SAS/projet_fin_sas.js
@@ -3,7 +3,7 @@ const prompt = require("prompt-sync")();
 // Déclaration d'un tableau Livers
 let livres = [
     { id_livre: 1, titre: "Le Petit Prince", auteur: "Saint-Exupéry", annee: 1943, disponible: true },
-    { id_livre: 2, titre: "ztranger", auteur: "Camus", annee: 1942, disponible: false }
+    { id_livre: 2, titre: "tranger", auteur: "Camus", annee: 1942, disponible: false }
 ];
 
 
@@ -184,6 +184,7 @@ function menu() {
     console.log("1. Introduire un livre");
     console.log("2. Ajouter plusieurs livres");
     console.log("3. Opérations sur les livres");
+    console.log("4. Gestion des abonnés");
     console.log("0. Quitter");
 
     let choix = Number(prompt("Entre choix : "))
@@ -234,8 +235,74 @@ function menuliver() {
 
 }
 
+//Gestion des abonnés
+// Déclaration d'un tableau abonnes
+let abonnes = [
+    { id: 1, nom: "outla", prenom: "zakaria", email: "zakariaoutla@mail.com" }
+];
 
 
+function gestionabonnes() {
+    let choix;
+    do {
+        console.log("===== Gestion des abonnés =====");
+        console.log("1. Ajouter un abonné");
+        console.log("2. Afficher tous les abonnés.");
+        console.log("3. Retour au menu principal");
+
+        choix = Number(prompt("Entre choix : "))
+
+        switch (choix) {
+            case 1:
+                ajouteabonnes()
+                break
+            case 2:
+                   showabonne()
+                break
+            case 3:
+                console.log("Retour au menu principal");
+                break
+            default:
+                console.log("Choix invalide")
+                break
+
+        }
+
+    } while (choix == !3)
+}
+// Cette fonction permet d'ajouter un nouveau abonnes
+function ajouteabonnes() {
+
+    let id = Number(prompt(`Entrez le id du abonne: `))
+    let nom = prompt(`Entrez le nom du abonne: `);
+    let prenom = prompt(`Entrez prenom du abonne: `);
+    let email = prompt(`Entrez email du abonne: `);
+    console.log("abonne ajoutée avec succès !");
+    abonnes.push(
+        {
+            id: id,
+            nom: nom,
+            prenom: prenom,
+            email: email,
+        }
+
+    )
+}
+
+function showabonne() {
+    if (abonnes.length === 0) {
+        console.log("aucun livers");
+
+    } else {
+        for (let i = 0; i < abonnes.length; i++) {
+            console.log(`${i + 1}: ${abonnes[i].id}`);
+            console.log(`nom: ${abonnes[i].nom}`);
+            console.log(`prenom: ${abonnes[i].prenom}`);
+            console.log(`email: ${abonnes[i].email}`);
+
+        }
+    }
+}
 
 
 
@@ -257,10 +324,16 @@ function run() {
             case 3:
                 menuliver()
                 break
+            case 4:
+                gestionabonnes()
+                break
+
             case 0:
                 console.log("Quitter");
+                break
 
             default:
+                console.log("Choix invalide")
                 break;
         }
 
